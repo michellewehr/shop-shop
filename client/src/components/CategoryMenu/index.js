5174b2723c8321d24048fc573e7abe6cb02a8df2
Michelle Wehr <michelle_wehr@icloud.com>
2021-12-14 12:06:32 -0500

    added indexdb

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -4,6 +4,7 @@ import { QUERY_CATEGORIES } from '../../utils/queries';
 import { useStoreContext } from "../../utils/GlobalState";
 import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
 
+import { idbPromise } from '../../utils/helpers';
 
 
 function CategoryMenu() {
@@ -11,19 +12,28 @@ function CategoryMenu() {
 
   const { categories } = state;
   
-  const { data: categoryData } = useQuery(QUERY_CATEGORIES);
+  const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
 
   useEffect(() => {
     //if categoryData exists or has changed from the response of useQuery, then run dispatch()
-    if(categoryData) {
-      //execute our dispatch function with our action object indicating the type of action and the data to set our state for categories to
+    if (categoryData) {
       dispatch({
         type: UPDATE_CATEGORIES,
         categories: categoryData.categories
       });
+      categoryData.categories.forEach(category => {
+        idbPromise('categories', 'put', category);
+      });
+    } else if (!loading) {
+      idbPromise('categories', 'get').then(categories => {
+        dispatch({
+          type: UPDATE_CATEGORIES,
+          categories: categories
+        })
+      })
     }
     //^^the useEffect() Hook notices that categoryData is not undefined anymore and runs the dispatch() function, setting our category data to the global state!
-  }, [categoryData, dispatch]);
+  }, [categoryData, loading, dispatch]);
 
   const handleClick = id => {
     dispatch({
